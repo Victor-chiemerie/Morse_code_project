068b6ba4fdab6c9bf99f7c91de3f359331dba99f
Ismail mayito <ismayito1@gmail.com>
2023-11-03 18:31:07 +0300

    Fix linter errors

--- decode.rb
+++ decode.rb
@@ -1,23 +1,30 @@
-# frozen_string_literal: true
+# Define the Morse code to character dictionary
+morse_dict = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z', '-----' => '0', '.----' => '1', '..---' => '2',
+  '...--' => '3', '....-' => '4', '.....' => '5', '-....' => '6',
+  '--...' => '7', '---..' => '8', '----.' => '9'
+}
 
-def morse_code_to_text(morse_code)
-  morse_dict = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z', '-----' => '0', '.----' => '1', '..---' => '2',
-    '...--' => '3', '....-' => '4', '.....' => '5', '-....' => '6',
-    '--...' => '7', '---..' => '8', '----.' => '9'
-  }
-
-  words = morse_code.split('   ') # Words are separated by three spaces
-  decoded_message = words.map do |word|
-    characters = word.split(' ') # Characters within words are separated by one space
-    characters.map { |char| morse_dict[char] }.join('')
-  end
-
-  decoded_message.join(' ') # Join words with single space and return the decoded message
+def morse_code_to_text(morse_code, morse_dict)
+  words = split_into_words(morse_code)
+  decoded_words = words.map { |word| decode_word(word, morse_dict) }
+  decoded_words.join(' ')
 end
-puts morse_code_to_text('... --- ...')
+
+def split_into_words(morse_code)
+  morse_code.split('   ') # Words are separated by three spaces
+end
+
+def decode_word(word, morse_dict)
+  characters = word.split(' ') # Characters within words are separated by one space
+  decoded_characters = characters.map { |char| morse_dict[char] }
+  decoded_characters.join('')
+end
+
+# Example usage
+puts morse_code_to_text('... --- ...', morse_dict) # This will print "SOS"
